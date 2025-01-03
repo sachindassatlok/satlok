cf109c719a60da132e24af612558bc046c909c26
sachindassatlok <sachinselwal@Sachins-MacBook-Air.local>
2025-01-03 18:08:33 +0530

    Add no-index configuration with robots.txt and meta tags

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="robots" content="noindex,nofollow" />
+        <meta name="googlebot" content="noindex,nofollow" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
